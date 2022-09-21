327fe20dd5e294a8e7c2deea6e86020bb9f7f4c1
Danny Briskin <dbriskin@qaconsultants.com>
2022-09-21 11:36:23 -0400

    [+] ToyshopCustomerAdditionalService - new service (just REST) [*] ToyShopService - was reverted to mimic original one

---
+++ toyShopCustomerAdditionalService/src/main/java/ua/com/univerpulse/toyshop/ToyshopCustomerAdditionalService.java
@@ -0,0 +1,14 @@
+package ua.com.univerpulse.toyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableOAuth2Sso
+public class ToyshopCustomerAdditionalService {
+    public static void main(String[] args) {
+        SpringApplication.run(ToyshopCustomerAdditionalService.class, args);
+    }
+}
---
+++ toyShopCustomerAdditionalService/src/main/java/ua/com/univerpulse/toyshop/config/security/ResourceServerConfig.java
@@ -0,0 +1,19 @@
+package ua.com.univerpulse.toyshop.config.security;
+
+/**
+ * @author Danny Briskin (DBriskin@qaconsultants.com)
+ * for toyShopCustomerService project.
+ */
+//@Configuration
+//@EnableResourceServer
+public class ResourceServerConfig
+//        extends ResourceServerConfigurerAdapter
+{
+//
+//    @Override
+//    public void configure(@NotNull HttpSecurity http) throws Exception {
+//        http
+//                .requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
+//                .authorizeRequests().anyRequest().fullyAuthenticated();
+//    }
+}
